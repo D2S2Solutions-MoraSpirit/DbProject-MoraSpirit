7f676e8581208dc64ffd95e4f7fc50461399bc69
Heshan-Sandamal <mhsandamal1@gmail.com>
2015-12-14 03:51:34 +0530

    adding to private directory works,jquery works and new functions added

--- src/application/StudentBundle/Resources/public/js/javascript.js
+++ src/application/StudentBundle/Resources/public/js/javascript.js
@@ -1,10 +1,26 @@
-/**
- * Created by Heshan Sandamal on 12/13/2015.
- */
-
-
 function getStudentDetails(event){
     if(event.keyCode()==13){
         alert("tjos os pl");
     }
 }
+
+$(document).ready(function(){
+    $("label").click(function(){
+        alert("Text");
+    });
+
+    $(window).keydown(function(event){
+        if(event.keyCode == 13) {
+            event.preventDefault();
+            return false;
+        }
+    });
+
+    $("#studentID").keydown(function(event){
+        if(event.keyCode == 13) {
+            alert("tis is ok");
+        }
+    });
+
+
+});
