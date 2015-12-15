f2904cb22b8652ba691a2e080651e9a5bde474b1
unknown <tdiluksha@gmail.com>
2015-12-15 14:28:14 +0530

    change sport db access controller to addsportformcontroller

--- src/application/SportBundle/Controller/DefaultController.php
+++ src/application/SportBundle/Controller/DefaultController.php
@@ -2,10 +2,8 @@
 
 namespace application\SportBundle\Controller;
 
-use application\MainBundle\Controller\SportDBaccess;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use application\MainBundle\Controller as cont;
-use application\MainBundle\Resources\Entity as en;
+
 
 class DefaultController extends Controller
 {
@@ -14,18 +12,5 @@ class DefaultController extends Controller
         return $this->render('SportBundle:Default:index.html.twig');
     }
 
-    public function addSportAction(){
-        return $this->render('SportBundle:AddSport:addSport.html.twig');
-    }
 
-    public function saveSportAction(){
-        $spr = new en\Sport();
-
-        $spr->setName($_POST["sportname"]);
-        $spr->setSportId($_POST["sportid"]);
-
-        cont\SportDBaccess::saveSport($spr);
-        return $this->render('SportBundle:Message:dbsave.html.twig');
-
-    }
 }
