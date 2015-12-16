45b42f24427d67184aeb6152a1b434647c71a01b
Sineth-N <sinethneranjana.13@cse.mrt.ac.lk>
2015-12-16 18:47:57 +0530

    GymSchedule added

--- src/application/SportBundle/Controller/ViewScheduleDataController.php
+++ src/application/SportBundle/Controller/ViewScheduleDataController.php
@@ -8,12 +8,18 @@
 
 namespace application\SportBundle\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ViewScheduleDataController extends Controller
 {
     public function viewScheduleAction(){
         return $this->render('SportBundle:Default:index.html.twig');
+    }
+    public function viewScheduleGymAction(){
+//        $resource_id=$_GET["resource_id"];
+//        $resource=cont\PracticeScheduleDBAccess::getAllLocationSchedule($resource_id);
+//        return new JsonResponse(['sport-id'=>$resource->getSportId(),'practise_date'=>$resource->getPractiseDate(),'practise_time'=>$resource->getPractiseTime(),'resource_id'=>$resource_id]);
+
     }
 }
