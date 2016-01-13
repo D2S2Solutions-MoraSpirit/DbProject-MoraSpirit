2c44eea6191c5a3411d94a9abc15bb24e370468d
dimuthnc <dimuthu.13@cse.mrt.ac.lk>
2016-01-14 01:02:40 +0530

    Template added

---
+++ src/application/MainBundle/Controller/MainTemplateController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dimuth Tharaka
+ * Date: 13/1/16
+ * Time: 11:48 PM
+ */
+
+namespace application\MainBundle\Controller;
+
+
+/*use Symfony\Component\HttpKernel\Tests\Controller;*/
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainTemplateController extends Controller{
+    public function templateTestAction(){
+        return $this->render('applicationMainBundle:Default:maintemplate.html.twig');
+    }
+    /*public function addCoachAction(){
+        return $this->render('SportBundle:AddCoach:addcoach.html.twig');
+    }*/
+
+
+}
