a6d91010dfe4ae3d873f3ebef75c0ae2361d49c5
Heshan-Sandamal <mhsandamal1@gmail.com>
2016-01-20 13:16:17 +0530

    modified SportInvolveController

--- src/application/StudentBundle/Controller/SportInvolveController.php
+++ src/application/StudentBundle/Controller/SportInvolveController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /**
  * Created by PhpStorm.
@@ -22,43 +21,12 @@ class SportInvolveController extends Controller
 
     public function indexAction()
     {
-        $sportList=SportDBaccess::getAllSports();
-        return $this->render('applicationStudentBundle:Default:AddStudentSPD.html.twig',array('sportList' => ($sportList)));
+        $sportList = SportDBaccess::getAllSports();
+        return $this->render('applicationStudentBundle:Default:AddStudentSPD.html.twig', array('sportList' => ($sportList)));
     }
 
-    public function addSportInvolveAction(SportInvolve $sportInvolve){
+    public function addSportInvolveAction(SportInvolve $sportInvolve)
+    {
         StudentDBaccess::addSportInvolve($sportInvolve);
     }
-=======
-<?php
-/**
- * Created by PhpStorm.
- * User: Heshan Sandamal
- * Date: 12/13/2015
- * Time: 2:58 PM
- */
-
-namespace application\StudentBundle\Controller;
-
-
-use application\MainBundle\Controller\SportDBaccess;
-use application\MainBundle\Controller\StudentDBaccess;
-use application\MainBundle\Resources\Entity\SportInvolve;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use application\MainBundle\Resources\Entity;
-
-class SportInvolveController extends Controller
-{
-
-
-    public function indexAction()
-    {
-        $sportList=SportDBaccess::getAllSports();
-        return $this->render('applicationStudentBundle:Default:AddStudentSPD.html.twig',array('sportList' => ($sportList)));
-    }
-
-    public function addSportInvolveAction(SportInvolve $sportInvolve){
-
-    }
->>>>>>> 66d486d01253d6575dffd1cc30354441d7b77312
 }
--- src/application/StudentBundle/Controller/StudentController.php
+++ src/application/StudentBundle/Controller/StudentController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace application\StudentBundle\Controller;
@@ -41,49 +40,4 @@ class StudentController extends Controller
 
     }
 
-
-
-
-
-
 }
-=======
-<?php
-
-namespace application\StudentBundle\Controller;
-use application\MainBundle\Resources\Entity;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use application\MainBundle\Controller as cont;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
-
-class StudentController extends Controller
-{
-    public function addStudentAction()
-    {
-        $student = new Entity\Student();
-        $student->setStudentId($_POST["student_id"]);
-        $student->setName($_POST["name"]);
-        $student->setFaculty($_POST["faculty"]);
-        $student->setBatch($_POST["batch"]);
-        $student->setAddress($_POST["address"]);
-        $student->setNic($_POST["nic"]);
-
-        cont\StudentDBaccess::addStudent($student);
-        return $this->render('applicationMainBundle:Default:index.html.twig');
-    }
-
-    public function getStudentDetailsAction(){
-        $student_id=$_GET["student_id"];
-        $student=cont\StudentDBaccess::getStudentDetails($student_id);
-        return new JsonResponse(['name'=>$student->getName(),'faculty'=>$student->getFaculty(),'batch'=>$student->getBatch()]);
-
-    }
-
-
-
-
-
-
-}
->>>>>>> 66d486d01253d6575dffd1cc30354441d7b77312
